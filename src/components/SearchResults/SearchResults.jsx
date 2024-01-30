47d3e806bbadd02f91deb07f7a6d00af729cd606
Aditya <adityakumarji74@gmail.com>
2024-01-31 01:00:40 +0530

    backend commit

--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -4,9 +4,17 @@ import Product from "../Product/Product";
 import { useLocation } from "react-router-dom";
 
 const SearchResults = () => {
+  // useLocation provides information about the current URL.
+  // const { search } = useLocation() extracts the query string from the URL, which includes parameters.
   const { search } = useLocation();
+
+  // URLSearchParams is a JavaScript API that makes it easy to work with query parameters.
+  //  It provides methods to manipulate the query string. (makes the object of query string)
   const queryParams = new URLSearchParams(search);
+
+  // queryParams.get() allows you to retrieve the value of a specific query parameter.
   const queryValue = queryParams.get("query");
+
   const { productList } = useContext(ProductListContext);
   return (
     <>
